1489f065b158722d4f2ea6cb0368bdaa561401bb
BlaqOswald <askelshafik@gmail.com>
2026-04-03 04:04:40 +0300

    fix database

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,8 @@
 import { Pool } from "pg";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
