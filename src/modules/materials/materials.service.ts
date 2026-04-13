7a60cb3edba6f443e562cd4894299a8184e4c6ff
BlaqOswald <askelshafik@gmail.com>
2026-04-13 17:27:45 +0300

    nemw

--- src/modules/materials/materials.service.ts
+++ src/modules/materials/materials.service.ts
@@ -1,37 +1,25 @@
 import { pool } from "../../db";
 
 export const addMaterial = async (data: any) => {
-  const {
-    project_id,
-    name,
-    unit_cost,
-    quantity_used,
-    total_cost,
-    currency,
-    description,
-    date_received,
-  } = data;
-
   const result = await pool.query(
     `INSERT INTO materials
     (project_id, name, unit_cost, quantity_used, total_cost, currency, description, date_received)
     VALUES ($1,$2,$3,$4,$5,$6,$7,$8)
     RETURNING *`,
     [
-      project_id,
-      name,
-      unit_cost,
-      quantity_used,
-      total_cost,
-      currency,
-      description,
-      date_received,
+      data.project_id,
+      data.name,
+      data.unit_cost,
+      data.quantity_used,
+      data.total_cost,
+      data.currency,
+      data.description || "",
+      data.date_received || null,
     ]
   );
 
   return result.rows[0];
 };
-
 export const getByProject = async (projectId: string) => {
   const result = await pool.query(
     `SELECT * FROM materials 
