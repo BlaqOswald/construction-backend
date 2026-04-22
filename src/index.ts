afc9e98565f57bb4a2082439dd9cf89919f69fe3
BlaqOswald <askelshafik@gmail.com>
2026-04-22 14:03:23 +0300

    server port 8080

--- src/index.ts
+++ src/index.ts
@@ -70,7 +70,7 @@ app.use(
 // ----------------------------------------------------
 // START SERVER
 // ----------------------------------------------------
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = Number(process.env.PORT) || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
