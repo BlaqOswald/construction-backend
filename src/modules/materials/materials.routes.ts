9f54797ee144aed993140df080bf4bb0945cf90f
BlaqOswald <askelshafik@gmail.com>
2026-05-11 16:25:57 +0300

    add

--- src/modules/materials/materials.routes.ts
+++ src/modules/materials/materials.routes.ts
@@ -1,9 +1,6 @@
 import { Router } from "express";
 import * as controller from "./materials.controller";
-
-// 🔥 IMPORT THESE
 import { verifyToken } from "../../middleware/auth.middleware";
-import { requireRole } from "../../middleware/role.middleware";
 
 const router = Router();
 
@@ -16,12 +13,7 @@ router.get("/project/:projectId", verifyToken, controller.getByProject);
 // UPDATE
 router.put("/:id", verifyToken, controller.updateMaterial);
 
-// DELETE (ADMIN ONLY 🔥)
-router.delete(
-  "/:id",
-  verifyToken,
-  requireRole("admin"),
-  controller.deleteMaterial
-);
+// DELETE
+router.delete("/:id", verifyToken, controller.deleteMaterial);
 
 export default router;
