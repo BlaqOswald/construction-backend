804c5c73cba9bb0882e87c3cb255f18f200a3d27
BlaqOswald <askelshafik@gmail.com>
2026-05-15 17:16:30 +0300

    k

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,12 @@ const app = express();
  */
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://construction-frontend-hy0nhfu9r-blaqoswalds-projects.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
